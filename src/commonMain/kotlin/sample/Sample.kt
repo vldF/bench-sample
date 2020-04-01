7f9cdc35693ad890db2ac5eaf317ea42aac86e87
vldf <vladislavF7@gmail.com>
2020-04-02 02:48:32 +0300

    Trying to build graal-native-image

--- src/commonMain/kotlin/sample/Sample.kt
+++ src/commonMain/kotlin/sample/Sample.kt
@@ -1,17 +1,20 @@
+package sample/*
 package sample
 
 import kotlinx.benchmark.*
-
 
 @State(Scope.Benchmark)
 @OutputTimeUnit(BenchmarkTimeUnit.MILLISECONDS)
 @Measurement(time = 1, timeUnit = BenchmarkTimeUnit.SECONDS)
 @BenchmarkMode(Mode.Throughput)
+
 class Sample {
     @Benchmark
     fun main() {
-        2+7
+        print(2+7)
     }
 }
 
-@TargetClass(className = "")
+
+
+*/
